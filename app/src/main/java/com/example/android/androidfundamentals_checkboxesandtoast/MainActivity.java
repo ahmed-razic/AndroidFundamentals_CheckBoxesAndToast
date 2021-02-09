927dd7cc11011b203cbd1cae8f841a3b7ce1457e
ahmed-razic <ahmed.razic@gmail.com>
2021-02-09 16:18:39 +0100

    added layouts

--- app/src/main/java/com/example/android/androidfundamentals_checkboxesandtoast/MainActivity.java
+++ app/src/main/java/com/example/android/androidfundamentals_checkboxesandtoast/MainActivity.java
@@ -10,46 +10,38 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class MainActivity extends AppCompatActivity {
+
+    private String message = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LinearLayout linearLayout = findViewById(R.id.linearLayout1);
-        ArrayAdapter adapter = new ArrayAdapter();
+        CheckBox checkBox1 = findViewById(R.id.checkBox1);
+        CheckBox checkBox2 = findViewById(R.id.checkBox2);
+        CheckBox checkBox3 = findViewById(R.id.checkBox3);
+        CheckBox checkBox4 = findViewById(R.id.checkBox4);
+        CheckBox checkBox5 = findViewById(R.id.checkBox5);
 
 
-            @Override
-            public void setAdapter(Adapter adapter) {
-
-            }
-
-            @Override
-            public View getSelectedView() {
-                return null;
-            }
-
-            @Override
-            public void setSelection(int position) {
-
-            }
+        if (checkBox1.isChecked()) {
+            message = message + getString(R.string.chocolate_syrup);
+        } else if (checkBox2.isChecked()) {
+            message = message + getString(R.string.sprinkles);
+        } else if (checkBox3.isChecked()) {
+            message = message + getString(R.string.crushed_nuts);
+        } else if (checkBox4.isChecked()) {
+            message = message + getString(R.string.cherries);
+        } else if (checkBox5.isChecked()) {
+            message = message + getString(R.string.oreo_cookie_crumbles);
         }
-
-
     }
-
-
 
     public void showToast(View view) {
-    }
-
-
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
